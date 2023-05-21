a6fa5c58053b6cfbfcb600c2a26fa2f607f62a6f
David Kleinman <kldavidt@amazon.com>
2023-05-21 19:46:50 +0300

    feat: changed api to rest api

--- src/api.py
+++ src/api.py
@@ -1,31 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
 import flask
+from flask_restful import Api, Resource
 from gpt import GPTClient
 
 
-app = flask.Flask(__name__)
 HEADERS = ({
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
     "Accept-Language": "en-US, en;q=0.5"
 })
 
-def _parse_content():
-    url = str(flask.request.args.get("url"))
-    html = requests.get(url, headers=HEADERS).text
-    soup = BeautifulSoup(html, "html.parser")
-    return " ".join([paragraph.get_text() for paragraph in soup.find_all("p")])
+app = flask.Flask(__name__)
+api = Api(app)
 
-@app.route("/summarize", methods=["GET"])
-def summarize_policy():
-    gpt_client = GPTClient()
-    policy_content = _parse_content()
-    gpt_answer = gpt_client.answer(policy_content, "What types of data are being collected from users?")
-    response = flask.jsonify(
-        {"data": gpt_answer}
-    )
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
+class PPExtensionApi(Resource):
+    def __init__(self) -> None:
+        super().__init__()
+        self.gpt_client = GPTClient()
+
+    def _parse_content_from_url(self, url):
+        html = requests.get(url, headers=HEADERS).text
+        soup = BeautifulSoup(html, "html.parser")
+        return " ".join([paragraph.get_text() for paragraph in soup.find_all("p")])
+
+    def get(self):
+        url = flask.request.args.get("url", None)
+        policy_content = self._parse_content_from_url(url)
+        # gpt_answer = self.gpt_client.answer(policy_content, "What types of data are being collected from users?")
+        response = flask.jsonify(
+            {"data": policy_content}
+        )
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
+
+api.add_resource(PPExtensionApi, "/summarize")
 
 if __name__ == "__main__":
-    app.run(port=9000)
+    app.run(port=9000, debug=True)
