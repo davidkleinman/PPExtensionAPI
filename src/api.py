cd7f379a69795aad6170704fffc043c87b6e6e25
David Kleinman <kldavidt@amazon.com>
2023-08-13 12:40:41 +0300

    feat: changed get method to post + CORS

--- src/api.py
+++ src/api.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import flask
 from flask_restful import Api, Resource
+from flask_cors import CORS
 from gpt import GPTClient
 
 
@@ -12,6 +13,7 @@ HEADERS = ({
 
 app = flask.Flask(__name__)
 api = Api(app)
+cors = CORS(app)
 
 class PPExtensionApi(Resource):
     def __init__(self) -> None:
@@ -23,20 +25,19 @@ class PPExtensionApi(Resource):
         soup = BeautifulSoup(html, "html.parser")
         return " ".join([paragraph.get_text() for paragraph in soup.find_all("p")])
 
-    def get(self):
-        url = flask.request.args.get("url", None)
-        api_key = flask.request.args.get("apiKey", None)
-        organization_id = flask.request.args.get("orgId", None)
+    def post(self):
+        request_body = flask.request.json
+        url = request_body.get("url")
+        api_key = request_body.get("apiKey")
+        organization_id = request_body.get("orgId")
         policy_content = self._parse_content_from_url(url)
         if not self.gpt_client.validate_privacy_policy(policy_content, api_key, organization_id):
-            response = flask.Response("The webpage does not contain a privacy policy", 400)
-            response.headers.add("Access-Control-Allow-Origin", "*")
+            response = flask.Response("The webpage does not contain a privacy policy", 404)
             return response
         gpt_answer = self.gpt_client.summarize_policy(policy_content, api_key, organization_id)
         response = flask.jsonify(
             {"data": gpt_answer}
         )
-        response.headers.add("Access-Control-Allow-Origin", "*")
         return response
 
 api.add_resource(PPExtensionApi, "/summarize")
